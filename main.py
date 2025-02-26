326d333393f82b9a90244636897b14ee177db9ad
morjun <junmo2011@gmail.com>
2025-02-26 12:12:22 +0900

    Change on to left rihgt on

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def evaluate_model(model, test_loader, device, stats_frame, test_indices):
 
     eval_df['index'] = test_indices
 
-    final_df = eval_df.merge(stats_frame, on='index')
+    final_df = eval_df.merge(stats_frame, left_on='index', right_on='index')
     print(final_df)
     accuracy = correct / total
     print(f"Test Accuracy: {accuracy:.2f}")
